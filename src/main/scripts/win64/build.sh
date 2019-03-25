1569b089e895629624e3beb89463d5a3d67f6d59
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-03-25 21:31:51 +0100

    Update build.sh

--- src/main/scripts/win64/build.sh
+++ src/main/scripts/win64/build.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
-mkdir ../../../../target/classes/win64
-
 cmake . -DCMAKE_BUILD_TYPE=Release -DCMAKE_COLOR_MAKEFILE=on -DLIBRARY_OUTPUT_PATH=" ../../../../target/classes/win64" -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
 
 make
 r1=$?
 
-cp physicsfs/win64/bin/libphysfs.dll ../../../../target/classes/win64
-cp ogre3d/win64/bin/OgreMain.dll ../../../../target/classes/win64
-cp ogre3d/win64/bin/OgreOverlay.dll ../../../../target/classes/win64
-cp ogre3d/win64/bin/Plugin_ParticleFX.dll ../../../../target/classes/win64
-cp ogre3d/win64/bin/RenderSystem_GL.dll ../../../../target/classes/win64
+cp physicsfs/win64/bin/libphysfs.dll ../../../../target/classes/win64/libphysfs.dll
+cp ogre3d/win64/bin/OgreMain.dll ../../../../target/classes/win64/OgreMain.dll
+cp ogre3d/win64/bin/OgreOverlay.dll ../../../../target/classes/win64/OgreOverlay.dll
+cp ogre3d/win64/bin/Plugin_ParticleFX.dll ../../../../target/classes/win64/Plugin_ParticleFX.dll
+cp ogre3d/win64/bin/RenderSystem_GL.dll ../../../../target/classes/win64/RenderSystem_GL.dll
 
 exit $r1
 
