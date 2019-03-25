cc9a366a57a4b82e560513e911567a882130192f
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-03-25 21:03:07 +0100

    Update build.sh

--- src/main/scripts/win64/build.sh
+++ src/main/scripts/win64/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+mkdir ../../../../target/classes/win64
+
 cmake . -DCMAKE_BUILD_TYPE=Release -DCMAKE_COLOR_MAKEFILE=on -DLIBRARY_OUTPUT_PATH=" ../../../../target/classes/win64" -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
 
 make
